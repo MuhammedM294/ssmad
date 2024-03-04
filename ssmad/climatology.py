b8f5f8c23d389acc42aa52030b5567d65b33da62
MuhammedM294 <muhammedaabdelaal@gmail.com>
2024-03-04 21:33:55 +0100

    Refactoring climatology module, edit add docsring

--- ssmad/climatology.py
+++ ssmad/climatology.py
@@ -1,5 +1,6 @@
 """
-A module for calculating climatology based on aggregation.
+A module for calculating climatology for different time steps (month, dekad, week) 
+using different metrics (mean, median, min, max).
 """
 
 __author__ = "Muhammed Abdelaal"
@@ -16,12 +17,16 @@ class Aggregation:
     -----------
     df : pd.DataFrame
         The DataFrame containing the data to be aggregated.
+        
     variable : str
         The variable/column in the DataFrame to be aggregated.
+        
     time_step : str
         The time step for aggregation. Supported values: 'month', 'dekad', 'week'.
+        
     metrics : List[str]
         The list of metrics to be applied during aggregation. Supported values: 'mean', 'median', 'min', 'max'.
+        
     resulted_df : pd.DataFrame
         The resulting DataFrame after aggregation.
 
@@ -29,46 +34,100 @@ class Aggregation:
     --------
     aggregate():
         Aggregates the data based on the specified time step and metrics.
+    
+    _validate_time_step():
+        Validates the time step.
+    
+    _validate_variable():
+        Validates the variable.
+        
+    _validate_input():
+        Validates the input parameters.
     """
-    def __init__(self, df: pd.DataFrame, variable: str, time_step: str, metrics: List[str]):
-        """
-        Initializes the Aggregation class.
+    def __init__(self, df: pd.DataFrame, variable: str, time_step: str):
+        """Initializes the Aggregation class.
 
         Parameters:
         -----------
         df : pd.DataFrame
             The DataFrame containing the data to be aggregated.
+            
         variable : str
             The variable/column in the DataFrame to be aggregated.
+            
         time_step : str
             The time step for aggregation. Supported values: 'month', 'dekad', 'week'.
+            
         metrics : List[str]
             The list of metrics to be applied during aggregation. Supported values: 'mean', 'median', 'min', 'max'.
+            
         """
-        self.df = df
+        self.original_df = df
         self.variable = variable
         self.time_step = time_step
-        self.metrics = metrics
+        self.valid_time_steps = ["month", "dekad", "week"]
+        self._validate_input()
+        # Resampling the data to daily frequency and removing NaN values
+        self.df = pd.DataFrame(self.original_df[self.variable]).resample('D').mean().dropna()
+        # Creating an empty DataFrame to store the aggregated data
         self.resulted_df = pd.DataFrame()
+        
+    def _validate_time_step(self,) -> None:
+        """
+        Validates the time step.
+        
+        Raises:
+        -------
+        ValueError:
+            If the time step is not one of the supported values.
+            
+        """
+        if self.time_step not in self.valid_time_steps:
+            raise ValueError(f"Invalid time step '{self.time_step}'. Supported values: {self.valid_time_steps}.")
+        
+    def _validate_variable(self):
+        """
+        Validates the variable to be aggregated.
+        
+        Raises:
+        -------
+        ValueError:
+            If the variable is not found in the input DataFrame columns.
+            
+        """
+        if self.variable not in self.original_df.columns:
+            raise ValueError(f"Variable '{self.variable}' not found in the input DataFrame columns.")
+    
+    def _validate_input(self):
+        """
+        Validates the input parameters.
+        
+        Raises:
+        -------
+        ValueError:
+            If the time step or variable is invalid.
+
+        """
+        self._validate_time_step()
+        self._validate_variable()
 
     def aggregate(self):
         """
-        Aggregates the data based on the specified time step and metrics.
+        Aggregates the data based on the specified .
         """
         pass
 
 
 class MonthlyAggregation(Aggregation):
     """
-    Aggregates the data based on month-based time step.
+    Aggregates the time series data based on month-based time step.     
     """
 
-    def __init__(self, df, variable, time_step, metrics):
-        super().__init__(df, variable, time_step, metrics)
+    def __init__(self, df, variable):
+        super().__init__(df, variable, 'month')
 
     def aggregate(self):
-        for metric in self.metrics:
-            self.resulted_df[metric] = self.df.groupby([self.df.index.year, self.df.index.month]).transform(metric)
+        self.resulted_df[f"{self.variable}-avg"] = self.df.groupby([self.df.index.year, self.df.index.month])[self.variable].transform('mean')
         return self.resulted_df
 
 class DekadalAggregation(Aggregation):
@@ -76,77 +135,73 @@ class DekadalAggregation(Aggregation):
     Aggregates the data based on dekad-based time step.
     """
 
-    def __init__(self, df, variable, time_step, metrics):
-        super().__init__(df, variable, time_step, metrics)
+    def __init__(self, df, variable):
+        super().__init__(df, variable, 'dekad')
 
     def aggregate(self):
         self.df['dekad'] = self.df.index.map(lambda x: 5 if x.day <= 10 else 15 if x.day <= 20 else 25)
-        for metric in self.metrics:
-            self.df[metric] = self.df.groupby([self.df.index.year, self.df.index.month, self.df['dekad']])[self.variable].transform(metric)
-        self.df = self.df.drop(columns=self.variable)
-        return self.df
+        self.resulted_df['dekad'] = self.df['dekad']
+        self.resulted_df[f"{self.variable}-avg"] = self.df.groupby([self.df.index.year, self.df.index.month, self.df['dekad']])[self.variable].transform('mean')
+        return self.resulted_df
 
 class WeeklyAggregation(Aggregation):
     """
-    Aggregates the data based on week-based time step.
+    Aggregates the time series data based on week-based time step.
     """
 
-    def __init__(self, df, variable, time_step, metrics):
-        super().__init__(df, variable, time_step, metrics)
+    def __init__(self, df, variable):
+        super().__init__(df, variable, 'week')
 
     def aggregate(self):
-        for metric in self.metrics:
-            self.resulted_df[metric] = self.df.groupby([self.df.index.year, self.df.index.isocalendar().week]).transform(metric)
+        self.resulted_df[f"{self.variable}-avg"] = self.df.groupby([self.df.index.year, self.df.index.isocalendar().week])[self.variable].transform('mean')
         return self.resulted_df
 
 class Aggregator(Aggregation):
     """
-    Aggregates the data based on the specified time step and metrics.
+    Aggregates the time series data based on the specified time step.
 
     Methods:
     --------
     aggregate():
-        Aggregates the data based on the specified time step and metrics and removes duplicates.
+        Aggregates the data based on the specified time step and removes duplicates.
     """
-    def __init__(self, df, variable, time_step, metrics):
-        super().__init__(df, variable, time_step, metrics)
+    def __init__(self, df, variable, time_step):
+        super().__init__(df, variable, time_step)
+        
 
-    def aggregate(self):
+    def aggregate(self ):
         if self.time_step == 'month':
-            return MonthlyAggregation(self.df, self.variable, self.time_step, self.metrics).aggregate().drop_duplicates()
+            return MonthlyAggregation(self.df, self.variable).aggregate().drop_duplicates()
         elif self.time_step == 'dekad':
-            return DekadalAggregation(self.df, self.variable, self.time_step, self.metrics).aggregate().drop_duplicates()
+            return DekadalAggregation(self.df, self.variable).aggregate().drop_duplicates()
         elif self.time_step == 'week':
-            return WeeklyAggregation(self.df, self.variable, self.time_step, self.metrics).aggregate().drop_duplicates()
-        else:
-            raise ValueError("Invalid time step. Please choose from 'month', 'dekad', 'week'")
-
-
+            return WeeklyAggregation(self.df, self.variable).aggregate().drop_duplicates()
+        
 
 
 class Climatology(Aggregation):
     """
-    A class for calculating climatology based on aggregation.
+    A class for calculating climatology(climate normal) for time series data.
 
     Attributes:
     ----------
     df_original: pd.DataFrame
-        The original input DataFrame.
+        The original input DataFrame before resampling and removing NaN values.
 
     df: pd.DataFrame
-        The input DataFrame containing the data to be aggregated.
+        The input DataFrame containing the preprocessed data to be aggregated.
 
     variable: str
         The variable/column in the DataFrame to be aggregated.
 
     time_step: str
-        The time step for aggregation ('month', 'dekad', or 'week').
+        The time step for aggregation. Supported values: 'month', 'dekad', 'week'.
 
     metrics: List[str]
         The list of metrics to be applied during aggregation. Supported values: 'mean', 'median', 'min', 'max'.
 
     resulted_df: pd.DataFrame
-        The resulting DataFrame after aggregation.
+        The resulting DataFrame storing the aggregated data.
 
     climatology_df: pd.DataFrame
         The DataFrame containing climatology information.
@@ -157,7 +212,7 @@ class Climatology(Aggregation):
         Aggregates the data based on the time step and metrics provided.
 
     filter_df:
-        Filters the DataFrame based on specified conditions.
+        Filters the DataFrame based on specified time/date conditions.
 
     climatology:
         Calculates climatology based on the aggregated data.
@@ -167,10 +222,30 @@ class Climatology(Aggregation):
         """
         Initializes the Climatology class.
         """
-        super().__init__(df, variable, time_step, metrics)
-        self.df_original = df
-        self.df = pd.DataFrame(df[variable]).resample("1D").mean().dropna()
+        self.metrics = metrics
+        self.valid_metrics = ["mean", "median", "min", "max"]
+        super().__init__(df, variable, time_step )
         self.climatology_df = pd.DataFrame()
+        
+        
+    def _validate_metrics(self):
+        """
+        Validates the metrics to be used in the climatology computation.
+
+        Raises:
+            ValueError: If the metric is not one of the supported values.
+            
+        """
+        for metric in self.metrics:
+            if metric not in self.valid_metrics:
+                raise ValueError(f"Invalid metric '{metric}'. Supported values: {self.valid_metrics}.")
+            
+    def _validate_input(self):
+        super()._validate_input()
+        self._validate_metrics()
+          
+    def __repr__(self):
+        return f"Climatology(df={self.original_df}, variable={self.variable}, time_step={self.time_step}, metrics={self.metrics})"
 
     @staticmethod
     def _filter_df(df: pd.DataFrame = None, year: Union[int, None] = None,
@@ -178,7 +253,7 @@ class Climatology(Aggregation):
                    week: Union[int, None] = None, start_date: Union[str, None] = None,
                    end_date: Union[str, None] = None) -> pd.DataFrame:
         """
-        Filters the DataFrame based on specified conditions.
+        Filters the DataFrame based on specified time/date conditions.
 
         Parameters:
         -----------
@@ -207,6 +282,7 @@ class Climatology(Aggregation):
         --------
         pd.DataFrame
             The filtered DataFrame.
+            
         """
         if df is None:
             print("No dataframe provided")
@@ -232,9 +308,37 @@ class Climatology(Aggregation):
     def aggregate(self):
         """
         Aggregates the data based on the time step and metrics provided.
+        
         """
-        return Aggregator(self.df, self.variable, self.time_step, self.metrics).aggregate()
+        return Aggregator(self.df, self.variable, self.time_step).aggregate()
     
+    
+    def _group_by(self , df):
+        """
+        Groups the DataFrame based on the provided time step.
+        
+        parameters:
+        ----------
+        
+        df: pd.DataFrame
+            The DataFrame to be grouped.
+        
+        returns:
+        --------
+        list
+            The list of date parameters to be used for grouping.
+        """
+        
+        if self.time_step == 'month':
+            return [df.index.month]
+        
+        elif self.time_step == 'week':
+            return [df.index.isocalendar().week]
+        
+        elif self.time_step == 'dekad':
+            return [df['dekad'] , df.index.month]
+        
+
     def climatology(self, **kwargs) -> pd.DataFrame:
         """
         Calculates climatology based on the aggregated data.
@@ -242,7 +346,7 @@ class Climatology(Aggregation):
         Parameters:
         -----------
         kwargs:
-            Additional time filtering parameters.
+            Additional time/date filtering parameters.
 
         Returns:
         --------
@@ -250,16 +354,16 @@ class Climatology(Aggregation):
             The DataFrame containing climatology information.
         """
         self.climatology_df = self.aggregate()
+        groupby_param = self._group_by(self.climatology_df)
         
         for metric in self.metrics:
-            if self.time_step == 'month':
-                self.climatology_df[f'normal_{metric}'] = self.climatology_df.groupby([self.climatology_df.index.month])[metric].transform(metric)
-            elif self.time_step == 'week':
-                self.climatology_df[f'normal_{metric}'] = self.climatology_df.groupby([self.climatology_df.index.isocalendar().week])[metric].transform(metric)
-            elif self.time_step == 'dekad':
-                self.climatology_df[f'normal_{metric}'] = self.climatology_df.groupby([self.climatology_df.index.month, self.climatology_df['dekad']])[metric].transform(metric)
+            self.climatology_df[f'normal-{metric}'] = self.climatology_df.groupby(groupby_param)[f"{self.variable}-avg"].transform(metric)
+            
         
         return self._filter_df(self.climatology_df, **kwargs)
+    
+
+
 
 
 if __name__ == "__main__":
@@ -276,5 +380,5 @@ if __name__ == "__main__":
     sm_ts = extract_obs_ts((lon, lat), ascat_path, obs_type="sm" , read_bulk=False)["ts"]
 
     
-    climatology = Climatology(sm_ts, "sm", "month", ["mean" , "median"])
+    climatology = Climatology(sm_ts, "sm", "dekad", ["mean",'median','min','max'])
     print(climatology.climatology(month = 2))
