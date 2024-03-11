10fd92a21f942de6b274cdc802309e3015078f66
MuhammedM294 <muhammedaabdelaal@gmail.com>
2024-03-11 14:43:17 +0100

    inital workflow testing

--- ssmad/run_workflow.py
+++ ssmad/run_workflow.py
@@ -7,48 +7,59 @@ __email__ = "muhammedaabdelaal@gmail.com"
 
 import pandas as pd
 from pathlib import Path
-from ssmad.data_reader import extract_obs_ts
+from ssmad.data_reader import*
 from ssmad.anomaly import Z_Score
+from time import time
+import concurrent.futures
 
+def set_path():
 
-if __name__ == "__main__":
-    
-    # Example usage
-  
+    # austria = "/home/m294/VSA/Code/pointlist_AUT.csv"
+    Germany = "/home/m294/VSA/Code/pointlist_DEU.csv"
     ascat_path = Path("/home/m294/VSA/Code/datasets")
     
-    # Single point Morocco
-    lat = 33.201
-    lon = -7.373
-    # loc  = 3597650
-    # loc = (lon, lat)
-    # sm_ts =  extract_obs_ts(loc, ascat_path, obs_type="sm" , read_bulk=False)["ts"]
-    # df = pd.DataFrame(sm_ts)
-    # #Compute monthly-based anomalies for October 2022 using z-score method
-    # anomaly_df = Z_Score(df = df , 
-    #                      variable='sm', 
-    #                      time_step='month').detect_anomaly(year = 2022, month = 10)
-    # print(anomaly_df)
+    return ascat_path, Germany
 
+def load_pointlist(aoi):
+    pointlist = pd.read_csv(aoi)
+    return pointlist
 
-    # multiple grid data points list in Austria 
-    pointlist_path = "/home/m294/VSA/Code/pointlist_fibgrid_n6600000.csv" 
-    pointlist = pd.read_csv(pointlist_path)
-    pointlist = pointlist[:20]
-    # Test anomalies computation for multiple points
-    def compute_anomaly(gpi):
-        sm_ts =  extract_obs_ts(gpi, ascat_path, obs_type="sm" , read_bulk=False)["ts"]
+def run(gpi):
+    try:
+        ascat_ts = ascat_obj.read(gpi)
+        sm_ts = ascat_ts.get("sm")
         df = pd.DataFrame(sm_ts)
         anomaly_df = Z_Score(df = df , 
                             variable='sm', 
                             time_step='month').detect_anomaly(year = 2022, month = 10)
-        return anomaly_df['z_score'].values[0]
-    
-    from time import time
-    start_time = time()
-    pointlist['z_score'] = pointlist['point'].apply(lambda x: compute_anomaly(x))
-    print("Time taken: ", time() - start_time)
-    print(pointlist)
-    
         
+        return (gpi, anomaly_df['z_score'].values[0])
+    except IOError as e:
+        print(e)
+       
+
+if __name__ == "__main__":
+    
+    # Example usage
+    ascat_path, aoi = set_path()
+    ascat_obj = AscatData(ascat_path, False)
+    pointlist = load_pointlist(aoi)
+  
+    # notfoundgpi= pointlist[pointlist['point'] == 4949511]
+    # pointlist = pointlist[:10]
+    # pointlist = pd.concat([pointlist, notfoundgpi])
+   
+              
+  
+    start = time()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        result = executor.map(run, pointlist['point'])
+        
+    for r in result:
+        if r:
+             pointlist.loc[pointlist['point'] == r[0], 'z_score'] = r[1]
+    print("Time taken in seconds: ", time() - start)  
+    print(pointlist)
+    pointlist.to_csv('ssmad/DEU_zscore.csv') 
+
     
